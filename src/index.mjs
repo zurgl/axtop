c61135573725e7a0242c1bf4ad7676d07e384703
zurgl <elayar.yacine@gamil.com>
2023-04-09 15:40:31 +0200

    websocket on front

--- src/index.mjs
+++ src/index.mjs
@@ -16,17 +16,27 @@ function App(props) {
   `;
 }
 
-const update = async () => {
-  let response = await fetch("/api/cpus");
-  if (response.status !== 200) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
+// const update = async () => {
+//   let response = await fetch("/api/cpus");
+//   if (response.status !== 200) {
+//     throw new Error(`HTTP error! status: ${response.status}`);
+//   }
 
-  let json = await response.json();
+//   let json = await response.json();
 
-  render(html`<${App} cpus=${json}></${App}>`, document.body)
+//   render(html`<${App} cpus=${json}></${App}>`, document.body)
+// };
+
+// update();
+// setInterval(update, 200);
+
+let url = new URL("/realtime/cpus", window.location.href);
+// http => ws
+// https => wss
+url.protocol = url.protocol.replace("http", "ws");
+
+let ws = new WebSocket(url.href);
+ws.onmessage = (ev) => {
+  let json = JSON.parse(ev.data);
+  render(html`<${App} cpus=${json}></${App}>`, document.body);
 };
-
-update();
-setInterval(update, 200);
-
