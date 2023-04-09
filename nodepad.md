c6d7ff088a69d15ad05754b4a158b1ccedf4b0c5
zurgl <elayar.yacine@gamil.com>
2023-04-09 13:04:32 +0200

    Initail commit

---
+++ nodepad.md
@@ -0,0 +1,12 @@
+### Nodepad
+
+```bash
+cargo new axact
+cd axact
+code .
+cargo add tokio -F full
+cargo add axum
+git add .
+git commit -m "Initail commit"
+git push
+```
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use axum::{Server, Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(root_get));
+    let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
+    let addr = server.local_addr();
+    println!("Listening on {addr}");
+
+    server.await.unwrap();
+}
+
+
+async fn root_get() -> &'static str {
+    "hello from Axum!"
+}
